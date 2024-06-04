685c06e36fc35536a1b2a7ca2ff38202d8d59bc3
Joel <joelfigueiridomolares@gmail.com>
2024-06-04 23:33:16 +0200

    Refactor(Item.java):Mapeado de clase

--- src/main/java/edu/badpals/Item.java
+++ src/main/java/edu/badpals/Item.java
@@ -1,11 +1,27 @@
 package edu.badpals;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "items")
+@NoArgsConstructor
 public class Item {
-
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public @Getter Long id;
+    
+    @Column(name = "item_nom")
     public String name;
-
+    @Column(name = "item_sellin")
     public int sellIn;
-
+    @Column(name = "item_quality")
     public int quality;
 
     public Item(String name, int sellIn, int quality) {
