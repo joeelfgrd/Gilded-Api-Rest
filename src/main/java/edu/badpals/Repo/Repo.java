2447e25127704c8465dccae2ad42fe8b75291b51
Joel <joelfigueiridomolares@gmail.com>
2024-06-04 23:35:31 +0200

    feat(repo.java):Metodos conexion a base de datos

---
+++ src/main/java/edu/badpals/Repo/Repo.java
@@ -0,0 +1,43 @@
+package edu.badpals.Repo;
+ 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import edu.badpals.Item;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class Repo {
+
+    @Inject
+    ItemRepo itemRepo;
+
+    public Repo(){}
+
+    public Item cargaItem(Long id){
+        Optional<Item> item = itemRepo.findByIdOptional(id);
+        return item.orElseGet(Item::new);
+    }
+
+    public Item cargaItem(String nombre){
+        return itemRepo.cargaItem(nombre);
+    }
+
+    public List<Item> cargaItems(String nombre){
+        return itemRepo.listAll().stream().filter(item -> item.name.equals(nombre)).toList();
+    }
+
+    @Transactional
+    public Item crearItem(Item item){
+        itemRepo.persist(item);
+        return this.cargaItem(item.getId());
+    }
+
+    @Transactional
+    public void deleteItem(Item item){
+        itemRepo.delete(item);
+    }
+}
