631ae42c6069c70d51782f8b06311995a80946b3
Joel <joelfigueiridomolares@gmail.com>
2024-06-05 13:23:24 +0200

    Refactor(ItemRepo.java):Implementar query en el metodo cargaItem

--- src/main/java/edu/badpals/Repo/ItemRepo.java
+++ src/main/java/edu/badpals/Repo/ItemRepo.java
@@ -1,12 +1,12 @@
 package edu.badpals.Repo;
 import edu.badpals.Item;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
-import java.util.List;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class ItemRepo implements PanacheRepositoryBase <Item, Long>{
     public Item cargaItem(String nombre){
-        List<Item> items = this.listAll();
-        return items.stream().filter(item -> item.name.equals(nombre)).findFirst().orElse(new Item());
+        return find("name", nombre).firstResult();
     }
 
 }
